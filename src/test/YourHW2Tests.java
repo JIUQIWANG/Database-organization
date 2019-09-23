b42f7b67260f5695b6a278091c845c62bf1f981d
Jiuqi Wang <wangjiuqi@wustl.edu>
2019-09-23 15:46:19 -0500

    add_test

--- src/test/YourHW2Tests.java
+++ src/test/YourHW2Tests.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +15,7 @@ import hw1.Catalog;
 import hw1.Database;
 import hw1.HeapFile;
 import hw1.TupleDesc;
+import hw2.Relation;
 
 public class YourHW2Tests {
 
@@ -25,33 +27,54 @@ public class YourHW2Tests {
 
 	@Before
 	public void setup() {
-		
+
 		try {
-			Files.copy(new File("testfiles/test.dat.bak").toPath(), new File("testfiles/test.dat").toPath(), StandardCopyOption.REPLACE_EXISTING);
-			Files.copy(new File("testfiles/A.dat.bak").toPath(), new File("testfiles/A.dat").toPath(), StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(new File("testfiles/test.dat.bak").toPath(), new File("testfiles/test.dat").toPath(),
+					StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(new File("testfiles/A.dat.bak").toPath(), new File("testfiles/A.dat").toPath(),
+					StandardCopyOption.REPLACE_EXISTING);
 		} catch (IOException e) {
 			System.out.println("unable to copy files");
 			e.printStackTrace();
 		}
-		
+
 		c = Database.getCatalog();
 		c.loadSchema("testfiles/test.txt");
-		
+
 		int tableId = c.getTableId("test");
 		testtd = c.getTupleDesc(tableId);
 		testhf = c.getDbFile(tableId);
-		
+
 		c = Database.getCatalog();
 		c.loadSchema("testfiles/A.txt");
-		
+
 		tableId = c.getTableId("A");
 		atd = c.getTupleDesc(tableId);
 		ahf = c.getDbFile(tableId);
 	}
-	
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDifferentTypesJoin() {
+		Relation tr = new Relation(testhf.getAllTuples(), testtd);
+		Relation ar = new Relation(ahf.getAllTuples(), atd);
+		tr = tr.join(ar, 1, 1);
+
+		assertTrue("There should be zero tuples after join", tr.getTuples().size() == 0);
+	}
+
+	@Test
+	public void testDuplicateColumsName() {
+		Relation ar = new Relation(ahf.getAllTuples(), atd);
+
+		ArrayList<Integer> f = new ArrayList<Integer>();
+		ArrayList<String> n = new ArrayList<String>();
+
+		f.add(0);
+		n.add("a2");
+
+		ar = ar.rename(f, n);
+		assertTrue("Rename should not remove any tuples", ar.getTuples().size() == 8);
+		assertTrue("Rename should not go through", ar.getDesc().getFieldName(0).equals("a1"));
 	}
 
 }
